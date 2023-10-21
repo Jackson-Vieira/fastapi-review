533335b3333aa083276da67fc30585a4b35dddad
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-21 00:56:24 -0300

    [chico-moedas] - add async converter

--- chico-moedas/converter.py
+++ chico-moedas/converter.py
@@ -1,5 +1,6 @@
 import os
 
+import aiohttp
 import requests
 from fastapi import HTTPException
 
@@ -31,6 +32,23 @@ def sync_converter(from_currency: str, to_currency: str, price: float):
         raise HTTPException(status_code=400, detail=error)
     
     data = response.json()
+    
+    if "Realtime Currency Exchange Rate" not in data:
+        raise HTTPException(status_code=500, detail='Realtime Currency not in the response')
+    
+    exchange_data = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
+    return exchange_data * price
+
+
+async def async_converter(from_currency: str, to_currency: str, price: float):
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={ALPHAVANTAGE_API_KEY}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=error)
     
     if "Realtime Currency Exchange Rate" not in data:
         raise HTTPException(status_code=500, detail='Realtime Currency not in the response')
