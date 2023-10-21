738d531032ffc9506a2088bab75db3d043708ec7
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-21 00:16:44 -0300

    [chico-moedas] - add sync converter handler

--- chico-moedas/routers.py
+++ chico-moedas/routers.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from converter import sync_converter
+
 router = APIRouter()
 
 # path param
@@ -8,6 +10,23 @@ router = APIRouter()
 # query param 
 # only add in functions params
 
+#/converter/?to_currencies=USD,EUR&price=5.55
+
 @router.get('/converter/{from_currency}')
 def converter(from_currency: str, to_currencies: str, price: float):
-    return True
+    to_currencies = to_currencies.split(",")
+
+    result = {
+        from_currency: {}
+    }
+
+    for currency in to_currencies:
+        response = sync_converter(
+            from_currency=from_currency,
+            to_currency=currency,
+            price=price
+        )
+
+        result[from_currency][currency] = response
+
+    return result
