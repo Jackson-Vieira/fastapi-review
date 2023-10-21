70474870df34d07a72c3668f91a46744d6723dc2
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-21 00:56:57 -0300

    [chico-moedas] - add async converter handler

--- chico-moedas/routers.py
+++ chico-moedas/routers.py
@@ -1,8 +1,10 @@
+from asyncio import gather
+
 from fastapi import APIRouter
 
-from converter import sync_converter
+from converter import async_converter, sync_converter
 
-router = APIRouter()
+router = APIRouter(prefix='/converter')
 
 # path param
 # router.get('/convert/from_currency')
@@ -12,14 +14,14 @@ router = APIRouter()
 
 #/converter/?to_currencies=USD,EUR&price=5.55
 
-@router.get('/converter/{from_currency}')
+@router.get('/{from_currency}')
 def converter(from_currency: str, to_currencies: str, price: float):
-    to_currencies = to_currencies.split(",")
 
     result = {
         from_currency: {}
     }
 
+    to_currencies = to_currencies.split(",")
     for currency in to_currencies:
         response = sync_converter(
             from_currency=from_currency,
@@ -29,4 +31,30 @@ def converter(from_currency: str, to_currencies: str, price: float):
 
         result[from_currency][currency] = response
 
+    return result
+
+@router.get('/async/{from_currency}')
+async def async_converter_router(from_currency: str, to_currencies: str, price: float):
+    batch = []
+    result = {
+        from_currency: {}
+    }
+
+    async def xyz(from_currency, to_currency, price):
+        result = await async_converter(
+            from_currency=from_currency,
+            to_currency=currency,
+            price=price
+        )
+        return to_currency, result
+
+    to_currencies = to_currencies.split(",")
+    for currency in to_currencies:
+        batch.append(xyz(
+            from_currency=from_currency,
+            to_currency=currency,
+            price=price
+        ))
+
+    result[from_currency] = { to_currency:value for to_currency, value in await gather(*batch) }
     return result
