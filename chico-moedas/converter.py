e6304f7e69c70cb28d8db3779dde803ea4cea0f9
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-21 03:08:18 -0300

    [chico-moedas] - go to sleep

--- chico-moedas/converter.py
+++ chico-moedas/converter.py
@@ -1,12 +1,9 @@
 import os
 
 import aiohttp
-import requests
 from fastapi import HTTPException
 
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
-
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 
 """
 {
@@ -24,35 +21,20 @@ ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 }
 """
 
-def sync_converter(from_currency: str, to_currency: str, price: float):
-    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={ALPHAVANTAGE_API_KEY}'
-    try:
-        response = requests.get(url)
-    except Exception as error:
-        raise HTTPException(status_code=400, detail=error)
-    
-    data = response.json()
-    
-    if "Realtime Currency Exchange Rate" not in data:
-        raise HTTPException(status_code=500, detail='Realtime Currency not in the response')
-    
-    exchange_data = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
-
-    return exchange_data * price
-
-
 async def async_converter(from_currency: str, to_currency: str, price: float):
     url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={ALPHAVANTAGE_API_KEY}'
+
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 data = await response.json()
     except Exception as error:
         raise HTTPException(status_code=400, detail=error)
-    
+
+    # Throttling error
     if "Realtime Currency Exchange Rate" not in data:
         raise HTTPException(status_code=500, detail='Realtime Currency not in the response')
     
     exchange_data = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
 
-    return exchange_data * price
+    return {to_currency: exchange_data * price} 
--- chico-moedas/routers.py
+++ chico-moedas/routers.py
@@ -1,60 +1,37 @@
+import logging
 from asyncio import gather
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query
 
-from converter import async_converter, sync_converter
+from converter import async_converter
+from schemas import ConverterOutput
 
-router = APIRouter(prefix='/converter')
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger(__name__)
 
-# path param
-# router.get('/convert/from_currency')
 
-# query param 
-# only add in functions params
+router = APIRouter()
 
 #/converter/?to_currencies=USD,EUR&price=5.55
-
-@router.get('/{from_currency}')
-def converter(from_currency: str, to_currencies: str, price: float):
-
-    result = {
-        from_currency: {}
-    }
-
-    to_currencies = to_currencies.split(",")
-    for currency in to_currencies:
-        response = sync_converter(
-            from_currency=from_currency,
-            to_currency=currency,
-            price=price
-        )
-
-        result[from_currency][currency] = response
-
-    return result
-
-@router.get('/async/{from_currency}')
-async def async_converter_router(from_currency: str, to_currencies: str, price: float):
+@router.get('/converter/{from_currency}', response_model=ConverterOutput)
+async def async_converter_router(
+        from_currency: str = Path(max_length=3), 
+        to_currencies: str = Query(max_length=50), 
+        price: float = Query(gt=0)
+    ):
+    logger.info("start async converter")
     batch = []
-    result = {
-        from_currency: {}
-    }
-
-    async def xyz(from_currency, to_currency, price):
-        result = await async_converter(
-            from_currency=from_currency,
-            to_currency=currency,
-            price=price
-        )
-        return to_currency, result
 
     to_currencies = to_currencies.split(",")
     for currency in to_currencies:
-        batch.append(xyz(
+        batch.append(async_converter(
             from_currency=from_currency,
             to_currency=currency,
             price=price
         ))
 
-    result[from_currency] = { to_currency:value for to_currency, value in await gather(*batch) }
-    return result
+    result = await gather(*batch)
+    return ConverterOutput(
+        message="sucess",
+        data=result
+    ) 
