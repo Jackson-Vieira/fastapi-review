f3b6074a0e8df31979722596d096121e0dee0dc8
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-20 23:53:38 -0300

    [chico-moedas] - add path and query params

--- chico-moedas/routers.py
+++ chico-moedas/routers.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get('/convert')
-def convert():
+# path param
+# router.get('/convert/from_currency')
+
+# query param 
+# only add in functions params
+
+@router.get('/convert/{from_currency}')
+def convert(from_currency: str, to_currencies: str, price: float):
     return True
